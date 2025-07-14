842f8ce7609504d9c9499613fb5bda7347318aeb
yonitobi <tobias100.us@gmail.com>
2025-07-14 12:54:00 +0300

    add Ex3 solution

--- BlinkAnimation.java
+++ BlinkAnimation.java
@@ -1,27 +1,29 @@
 import java.awt.Color;
 import Utils.StopWatch;
-public class BlinkAnimation extends AnimationBase{
+
+public class BlinkAnimation extends AnimationBase {
     private Color color;
     private double interval;
     private StopWatch clock;
-    public BlinkAnimation(Color color){
+
+    public BlinkAnimation(Color color) {
         this.color = color;
         this.clock = new StopWatch();
         this.interval = 2.0;
     }
-    public void init(){
+
+    public void init() {
         this.clock.start();
     }
-    public void periodic(){
-        if(clock.get() % 2 == 0){
+
+    public void periodic() {
+        if ((int) clock.get() % 2 == 0) {
             this.strip.setAll(color);
-            this.strip.apply();
         }
-        else{
+        else {
             this.strip.setAll(Color.black);
-            this.strip.apply();
         }
-        if(clock.get() == interval){
+        if (clock.get() == interval) {
             clock.start();
         }
     }
